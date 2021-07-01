eda99dcc6225a1a2bba3ca404daf687accab3006
Mamata H <72901730+Mamata4225@users.noreply.github.com>
2021-07-01 19:40:03 +0530

    Add files via upload

---
+++ airtel-app/src/main/java/com/xorkz/airtel/AirtelServlet.java
@@ -0,0 +1,15 @@
+package com.xorkz.airtel;
+
+import java.io.IOException;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+public class AirtelServlet extends GenericServlet {
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+
+	}
+
+}
---
+++ airtel-app/src/main/java/com/xworkz/airtel/dto/AirtelDTO.java
@@ -0,0 +1,10 @@
+package com.xworkz.airtel.dto;
+
+public class AirtelDTO {
+
+	public AirtelDTO() {
+		System.out.println(this.getClass().getSimpleName() + " Object is Created ");
+	}
+
+	
+}
